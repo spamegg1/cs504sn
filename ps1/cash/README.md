1cf30fc752c8c6a45c8f88bb87f63e40fb14c0c9
spamegg <spamegg@pm.me>
2026-04-15 01:00:35 +0300

    ps1/cash

---
+++ ps1/cash/README.md
@@ -0,0 +1,22 @@
+# Cash
+
+Suppose you work at a store and a customer gives you $1.00 (100 cents) for candy
+that costs $0.50 (50 cents). You'll need to pay them their "change,"
+the amount leftover after paying for the cost of the candy.
+
+When making change, odds are you want to minimize the number of coins you're
+dispensing for each customer, lest you run out (or annoy the customer!).
+Implement a program that prints the minimum coins needed to
+make the given amount of change, in cents, as in the below:
+
+> Change owed: 25
+> 1
+
+But prompt the user for an int greater than 0,
+so that the program works for any amount of change:
+
+> Change owed: 70
+> 4
+
+Re-prompt the user, again and again as needed, if their input is not greater than
+or equal to 0 (or if their input isn't an int at all!).
---
+++ ps1/cash/cash.scala
@@ -0,0 +1,43 @@
+import scalanative.unsafe.{CQuote, stackalloc, Ptr, CInt}
+import scalanative.libc.stdio.{printf, sscanf, stdin, fgets}
+import scala.util.boundary, boundary.break
+
+/** Reads input from the user and parses it to an integer. If the input is not an integer, throws
+  * `IllegalArgumentException`.
+  *
+  * @return
+  *   The integer value inputted by the user.
+  */
+def getInt: Int =
+  val line: Ptr[Byte]      = stackalloc[Byte](1024)
+  val intPointer: Ptr[Int] = stackalloc[Int](1)
+  boundary:
+    while fgets(line, 1024 - 1, stdin) != null do
+      val scanResult: CInt = sscanf(line, c"%d\n", intPointer)
+      if scanResult == 0 then throw java.lang.IllegalArgumentException("parse error in sscanf, expected integer")
+      else break()
+  !intPointer
+
+/** Reads input from the user and parses it to a nonnegative integer.
+  *
+  * @return
+  *   The nonnegative integer value inputted by the user.
+  */
+def getCents: Int =
+  var cents = -1
+  while cents < 0 do
+    printf(c"Number of cents: ")
+    cents = getInt
+  cents
+
+@main
+def cash: Unit =
+  val cents    = getCents              // Ask how many cents the customer is owed
+  val quarters = cents / 25            // Calculate how many quarters to give the customer
+  val change1  = cents - quarters * 25 // Remove quarters
+  val dimes    = change1 / 10          // Calculate how many dimes to give the customer
+  val change2  = change1 - dimes * 10  // Remove dimes
+  val nickels  = change2 / 5           // Calculate how many nickels to give the customer
+  val pennies  = change2 - nickels * 5 // Remove nickels, what's left are pennies
+  val coins    = quarters + dimes + nickels + pennies
+  printf(c"%i\n", coins) // total number of coins to give the customer
