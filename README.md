dc9fc89d1c09a161d671c05bdc3784709356967f
spamegg <spamegg@pm.me>
2026-04-14 18:52:34 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CS504SN
+
+Adapting my C solutions to [CS50](https://cs50.harvard.edu/x/)
+programming assignments to [Scala Native](https://scala-native.org/)
---
+++ project.scala
@@ -0,0 +1,5 @@
+//> using scala 3.8.3
+//> using toolkit 0.9.2
+//> using platform native
+//> using nativeVersion 0.5.10
+//> using options -explain-cyclic -Ydebug-cyclic
