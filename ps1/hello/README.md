314fed27fb729561697bdd9e3902484ab75dd9d7
spamegg <spamegg@pm.me>
2026-04-16 22:44:46 +0300

    ps1 hello, reorganize folders

---
+++ ps1/hello/README.md
@@ -0,0 +1,16 @@
+# Hello, me
+
+Implement a program that prompts the user for their name and then says hello to that user.
+For instance, if the user’s name is Adele, your program should print `hello, Adele\n`!
+
+```bash
+$ ./hello
+What's your name? Adele
+hello, Adele
+$ ./hello
+What's your name?
+```
+
+Recall that you can get a string from a user with `getString`.
+Recall that you can print a string with `printf`, which is declared in `stdio`.
+Recall that you can format a string with `printf` with `%s`.
---
+++ ps1/hello/hello.scala
@@ -0,0 +1,34 @@
+import scalanative.unsafe.{CQuote, CString, stackalloc}
+import scalanative.libc.stdio.{printf, stdin, fgets, sscanf}
+import scalanative.libc.stdlib.malloc
+import scala.util.boundary, boundary.break
+import java.lang.IllegalArgumentException as IAE
+
+/** Gets input from user via `stdin` and returns the string the user inputted (pointer to the beginning of the string,
+  * allocated on the heap). Does not check if string length is safe, if `malloc` failed, if `fgets` failed or if
+  * `sscanf` failed. Assumes user input to be less than 256 bytes.
+  *
+  * @return
+  *   The `CString` user typed into `stdin`.
+  */
+def getString: CString =
+  val in  = stackalloc[Byte](256)
+  val out = malloc(256)
+  val _   = fgets(in, 256, stdin)
+  val _   = sscanf(in, c"%255s\n", out)
+  out
+
+/** Asks the user for their name and prints a greeting message. Example:
+  *
+  * ❯ ./hello
+  *
+  * What is your name?
+  *
+  * Spam
+  *
+  * hello, Spam
+  */
+@main
+def hello: Unit =
+  printf(c"What is your name?\n")
+  printf(c"hello, %s\n", getString);
