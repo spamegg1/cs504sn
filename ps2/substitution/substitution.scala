f07c1c7fb1cd84d8b50a52feb1df9956094a00eb
spamegg <spamegg@pm.me>
2026-04-20 19:47:02 +0300

    ps2 substitution cleanup and test

--- ps2/substitution/substitution.scala
+++ ps2/substitution/substitution.scala
@@ -25,78 +25,68 @@ def strToLower(key: CString): Unit =
   * @return
   *   true if key is invalid, false if key is good.
   */
-def invalid(key: CString): CBool =
-  boundary:
-    if strlen(key) != 26 then
-      printf(c"Key must contain 26 characters.\n")
+def invalid(key: CString): CBool = boundary:
+  if strlen(key) != 26 then
+    printf(c"Key must contain 26 characters.\n")
+    break(true)
+
+  for i <- 0 until 26 do
+    if isalpha(key(i)) == 0 then
+      printf(c"Key must contain only alphabetical characters.\n")
       break(true)
 
-    for i <- 0 until 26 do
-      if isalpha(key(i)) == 0 then
-        printf(c"Key must contain only alphabetical characters.\n")
-        break(true)
+  for i <- 0 until 26 do // check if key contains each lowercase letter exactly once
+    var count = 0
+    for j <- 0 until 26 do if Lower(i) == key(j) then count += 1
+    if count != 1 then
+      printf(c"Key must contain each letter exactly once.\n")
+      break(true)
 
-    // check if key contains each lowercase letter exactly once
-    for i <- 0 until 26 do
-      var count = 0
-      for j <- 0 until 26 do if Lower(i) == key(j) then count += 1
-      if count != 1 then
-        printf(c"Key must contain each letter exactly once.\n")
-        break(true)
+  break(false) // passed all checks, key is valid.
 
-    break(false) // passed all checks, key is valid.
+/** Writes ciphertext to given buffer, in place, based on given key.
+  *
+  * @param length
+  *   Length of the plaintext (and of the ciphertext).
+  * @param plaintext
+  *   A string of characters.
+  * @param ciphertext
+  *   Pre-allocated buffer to write into.
+  * @param key
+  *   A permutation of the 26-letter alphabet to be used as cipher.
+  */
+def substitute(length: CSize, plaintext: CString, ciphertext: CString, key: CString): Unit =
+  for i <- 0 until length.toInt do // Go through the plaintext
+    if isalpha(plaintext(i)) != 0 then   // check if the ith char is a letter
+      if isupper(plaintext(i)) != 0 then // Now check if it is upper case
+        boundary:
+          for k <- 0 until 26 do
+            if Upper(k) == plaintext(i) then         // Now find its position in the alphabet
+              ciphertext(i) = toupper(key(k)).toByte // replace it by letter in key
+              break()
+      if islower(plaintext(i)) != 0 then // Repeat same procedure for lower case
+        boundary:
+          for k <- 0 until 26 do
+            if Lower(k) == plaintext(i) then         // Now find its position in the alphabet
+              ciphertext(i) = tolower(key(k)).toByte // replace it by letter in key
+              break()
+    else ciphertext(i) = plaintext(i) // Finally, if it's not a letter, leave it unchanged
 
 object Substitution:
   def main(args: Array[String]): Unit =
     boundary:
-      // Give error if there is not exactly one command line argument
-      if args.length != 1 then
+      if args.length != 1 then // error if there is not exactly 1 argument
         printf(c"Usage: ./substitution key\n")
         break(EXIT_FAILURE)
 
       Zone:
-        // Store the key in a variable, convert all to lowercase
         val key = toCString(args.head)
         strToLower(key)
-        if invalid(key) then break(EXIT_FAILURE) // Give error if key is invalid
-
-        // Get from user the plaintext to be ciphered
-        val plaintext = getString(c"plaintext: ")
-        val length    = strlen(plaintext)
-
-        // Declare the ciphertext, terminate with the null character
-        val ciphertext = stackalloc[Byte](length + 1.toUSize)
-        ciphertext(length) = 0.toByte
-
-        // Go through the plaintext
-        for i <- 0 until length.toInt do
-          // First check if the ith character in plaintext is a letter
-          if isalpha(plaintext(i)) != 0 then
-            // Now check if it is upper case
-            if isupper(plaintext(i)) != 0 then
-              // Now find its position in the alphabet,
-              // replace it by corresponding letter in key
-              boundary:
-                for k <- 0 until 26 do
-                  if Upper(k) == plaintext(i) then
-                    ciphertext(i) = toupper(key(k)).toByte
-                    break()
-
-            // Repeat same procedure for lower case
-            if islower(plaintext(i)) != 0 then
-              // Now find its position in the alphabet,
-              // replace it by corresponding letter in key
-              boundary:
-                for k <- 0 until 26 do
-                  if Lower(k) == plaintext(i) then
-                    ciphertext(i) = key(k)
-                    break()
-
-          // Finally, if the ith character is not a letter, leave it unchanged
-          // Also we do not move forward in the shift array this time,
-          // as no letters from keyword were used. Variable j remains unchanged.
-          else ciphertext(i) = plaintext(i)
-
-        // Print the completed ciphertext
+        if invalid(key) then break(EXIT_FAILURE)
+        val plaintext  = getString(c"plaintext: ")
+        val length     = strlen(plaintext)
+        val ciphertext = stackalloc[Byte](length + 1.toUSize) // Allocate ciphertext
+        substitute(length, plaintext, ciphertext, key) // write substitution
+        ciphertext(length) = 0.toByte // null terminate
         printf(c"ciphertext: %s\n", ciphertext)
         break(EXIT_SUCCESS)
--- test/ps2/substitution.test.scala
+++ test/ps2/substitution.test.scala
@@ -1,6 +1,12 @@
-import scalanative.unsafe.CQuote
+import scalanative.unsafe.{CQuote, stackalloc, fromCString, Zone}
+import scalanative.unsigned.UnsignedRichInt
 import munit.FunSuite
 
 class SubstitutionTest extends FunSuite:
-  test("substitution"):
-    assert(true)
+  test("substitution works correctly on valid key and short plaintext"):
+    val key        = c"YTNSHKVEFXRBAUQZCLWDMIPGJO"
+    val plaintext  = c"Hello!"
+    val ciphertext = stackalloc[Byte](7)
+    substitute(7.toUSize, plaintext, ciphertext, key)
+    ciphertext(6) = 0.toByte
+    assertEquals(fromCString(ciphertext), "Ehbbq!")
