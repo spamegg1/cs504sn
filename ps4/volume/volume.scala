300da6223abfafe485df1450c017edf5beb7a71c
spamegg <spamegg@pm.me>
2026-05-09 20:53:17 +0300

    ps4 volume. Easy!

--- ps4/volume/volume.scala
+++ ps4/volume/volume.scala
@@ -1,11 +1,47 @@
 import scalanative.unsafe.*
-import scalanative.unsigned.{UnsignedRichInt, USize}
-import scalanative.libc.stdio.printf
-import scalanative.libc.string.strlen
-import scalanative.libc.ctype.{isalpha, isupper, islower, isspace, tolower, toupper}
-import scalanative.libc.math.round
-import scalanative.libc.stdlib.{EXIT_SUCCESS, EXIT_FAILURE}
+import scalanative.unsigned.*
+import scalanative.libc.stdio.*
+import scalanative.libc.stdint.*
+import scalanative.libc.stdlib.{EXIT_SUCCESS, EXIT_FAILURE, atof}
 import scala.util.boundary, boundary.break
 
-@main
-def volume: Unit = ()
+object Volume:
+  val HeaderSize = 44.toCSize // Number of bytes in .wav header
+
+  def main(args: Array[String]): Unit = boundary:
+    val argc = args.length
+    if argc != 3 then // Check command-line arguments
+      printf(c"Usage: ./volume input.wav output.wav factor\n")
+      break(EXIT_FAILURE)
+
+    Zone:
+      // Open files and determine scaling factor
+      val input: Ptr[FILE] = fopen(toCString(args.head), c"r")
+      if input == null then
+        printf(c"Could not open file.\n")
+        break(EXIT_FAILURE)
+
+      val output: Ptr[FILE] = fopen(toCString(args(1)), c"w")
+      if output == null then
+        printf(c"Could not open file.\n")
+        break(EXIT_FAILURE)
+
+      val factor = atof(toCString(args(2)))
+
+      // Copy header from input file to output file
+      val header = alloc[UByte](HeaderSize)
+      fread(header, HeaderSize, 1.toCSize, input)
+      fwrite(header, HeaderSize, 1.toCSize, output)
+
+      // Read samples from input file and write updated data to output file
+      val buffer = alloc[int16_t](1) // Create a buffer for a single sample
+
+      // Read single sample from input into buffer while there are samples left to read
+      while fread(buffer, sizeof[int16_t], 1.toCSize, input) != 0 do
+        val double = (!buffer).toDouble * factor
+        !buffer = double.toShort                           // Update volume of sample
+        fwrite(buffer, sizeof[int16_t], 1.toCSize, output) // Write updated sample to new file
+
+      fclose(input) // Close files
+      fclose(output)
+      EXIT_SUCCESS
