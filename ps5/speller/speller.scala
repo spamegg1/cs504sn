b5f223dec66f8723e83199375d896247849ed8bd
spamegg <spamegg@pm.me>
2026-05-12 13:05:36 +0300

    small refactors

--- ps5/speller/speller.scala
+++ ps5/speller/speller.scala
@@ -44,16 +44,15 @@ object Speller:
       break(EXIT_FAILURE)
 
     Zone:
-      val before     = alloc[rusage](1) // Structures for timing data
-      val after      = alloc[rusage](1)
-      var time_check = 0.0              // Benchmarks
+      val before = alloc[rusage](1) // Structures for timing data
+      val after  = alloc[rusage](1)
 
       // Load selected dictionary (or the default), time how long it takes to load it
       val dictionary = if argc == 2 then toCString(args.head) else DefaultDict
       getrusage(RUSAGE_SELF, before)
       val loaded = Dictionary.loadDict(dictionary)
       getrusage(RUSAGE_SELF, after)
-      val time_load = calcTimeDiff(before, after)
+      val timeLoad = calcTimeDiff(before, after)
 
       if !loaded then // Exit if dictionary not loaded
         printf(c"Could not load %s.\n", dictionary)
@@ -61,16 +60,17 @@ object Speller:
 
       val text = toCString(if argc == 2 then args(1) else args.head)
       val file = fopen(text, c"r") // Try to open text
-      if file == null then
+      if file == null then // exit if failed
         printf(c"Could not open %s.\n", text)
         break(EXIT_FAILURE)
 
       // printf(c"\nMISSPELLED WORDS\n\n") // Prepare to report misspell
 
-      var index    = 0 // Prepare to spell-check
-      var misspell = 0
-      var words    = 0
-      val word     = alloc[CChar](MaxLen + 1)
+      var index     = 0   // Prepare to spell-check
+      var misspell  = 0
+      var words     = 0
+      var timeCheck = 0.0 // Benchmarks
+      val word      = alloc[CChar](MaxLen + 1)
 
       val c = alloc[CChar](1) // Spell-check each word in text
       while fread(c, sizeof[CChar], 1.toCSize, file) != 0 do
@@ -79,21 +79,20 @@ object Speller:
           word(index) = !c // Append character to word
           index += 1
           if index > MaxLen then // Ignore alphabetical strings too long to be words
-            // Consume remainder of alphabetical string
+            // Consume remainder of alphabetical string, Prepare for new word
             while fread(c, sizeof[CChar], 1.toCSize, file) != 0 && isalpha(!c) != 0 do ()
-            index = 0                 // Prepare for new word
+            index = 0
         else if isdigit(!c) != 0 then // Ignore words with numbers (like MS Word can)
-          // Consume remainder of alphanumeric string
+          // Consume remainder of alphanumeric string, Prepare for new word
           while fread(c, sizeof[CChar], 1.toCSize, file) != 0 && isalnum(!c) != 0 do ()
-          index = 0              // Prepare for new word
-        else if index > 0 then   // We must have found a whole word
-          word(index) = 0.toByte // Terminate current word
-          words += 1             // Update counter
-
+          index = 0
+        else if index > 0 then           // We must have found a whole word
+          word(index) = 0.toByte         // Terminate current word
+          words += 1                     // Update counter
           getrusage(RUSAGE_SELF, before) // Check word's spelling
-          var misspelled = !Dictionary.check(word)
+          val misspelled = !Dictionary.check(word)
           getrusage(RUSAGE_SELF, after)
-          time_check += calcTimeDiff(before, after) // Update benchmark
+          timeCheck += calcTimeDiff(before, after) // Update benchmark
 
           if misspelled then // Print word if misspelled
             // printf(c"%s\n", word)
@@ -111,15 +110,15 @@ object Speller:
       getrusage(RUSAGE_SELF, before)
       val wordCount = Dictionary.getSize
       getrusage(RUSAGE_SELF, after)
-      val time_size = calcTimeDiff(before, after)
+      val timeSize = calcTimeDiff(before, after)
 
-      val total = time_load + time_check + time_size
+      val total = timeLoad + timeCheck + timeSize
       printf(c"\n")
       printf(c"WORDS MISSPELLED:     %d\n", misspell)
       printf(c"WORDS IN DICTIONARY:  %d\n", wordCount)
       printf(c"WORDS IN TEXT:        %d\n", words)
-      printf(c"TIME IN load:         %.2f\n", time_load)
-      printf(c"TIME IN check:        %.2f\n", time_check)
-      printf(c"TIME IN size:         %.2f\n", time_size)
+      printf(c"TIME IN load:         %.2f\n", timeLoad)
+      printf(c"TIME IN check:        %.2f\n", timeCheck)
+      printf(c"TIME IN size:         %.2f\n", timeSize)
       printf(c"TIME IN TOTAL:        %.2f\n\n", total)
       break(EXIT_SUCCESS)
